68a714813cef8bce120c54ae398f3012ff4b195f
NATÃ GABRIEL DE OLIVEIRA <nata_g_oliveira@estudante.sesisenai.org.br>
2025-05-06 11:27:59 -0300

    Progredindo na interatividade com JavaScript

--- js/login.js
+++ js/login.js
@@ -2,6 +2,7 @@
 
 let sign_in_button = document.querySelector('button#sign_in');
 let sign_up_button = document.querySelector('button#sign_up');
+let check_password_field = document.querySelector('div#check_password_field');
 
 const switchScreen = evt => {
     if(evt.target.id === 'sign_in') {
@@ -9,11 +10,15 @@ const switchScreen = evt => {
         sign_in_button.style.marginLeft = '2.96px';
         sign_up_button.classList.remove('active_button');
         sign_up_button.style.marginRight = 'auto';
+
+        check_password_field.style.display = 'none';
     } else {
         sign_up_button.classList.add('active_button');
         sign_up_button.style.marginRight = '2.96px';
         sign_in_button.classList.remove('active_button');
         sign_in_button.style.marginLeft = 'auto';
+
+        check_password_field.style.display = 'block';
     }
 }
 
