b2c4ea15d8eb14fc65964fd6d6f60b0c99cdac87
Olliver-0 <nata.gabriel.de.oliveira@gmail.com>
2025-06-19 19:47:28 -0300

    Variáveis de cores atualizadas

--- src/components/intro-section/intro-section-styles.js
+++ src/components/intro-section/intro-section-styles.js
@@ -1,6 +1,6 @@
 const styles = `
     section {
-        background-color: var(--primary-color-dark);
+        background-color: var(--primary-dark);
         padding: 20px;
         min-height: 300px;
         position: relative;
@@ -9,12 +9,12 @@ const styles = `
         justify-content: center; 
         align-items: center; 
         text-align: center;
-        overflow: hidden; /* Adicionado para conter os elementos posicionados */
+        overflow: hidden;
     }
 
     .boom {
         font: 400 42px/normal 'Bangers';
-        color: var(--tertiary-color);
+        color: var(--tertiary);
         text-shadow: 1px 2px 0px black;
         position: absolute;
         top: 20px;
@@ -23,7 +23,7 @@ const styles = `
 
     .wow {
         font: 400 42px/normal 'Bangers';
-        color: var(--base-color);
+        color: var(--base);
         text-shadow: 1px 2px 0px black;
         position: absolute;
         bottom: 20px;
@@ -34,25 +34,20 @@ const styles = `
         font: 400 58px/normal 'Bangers';
         text-shadow: 1px 2px 0px black; 
         margin-top: 10px;
-        line-height: 1; /* Ajuste para títulos grandes */
+        line-height: 1;
     }
     
     .main-title.p1 { 
-        color: var(--base-color);
+        color: var(--base);
     }
     
     .main-title.p2 { 
-        color: var(--tertiary-color);
-    }
-    
-    .main-title.p2 span { 
-        /* Esta regra estava vazia, pode ser removida ou utilizada */
-        /* Exemplo: color: var(--base-color); */
+        color: var(--tertiary);
     }
 
     .s-principal {
         font: 400 25px/normal 'Bangers';
-        color: var(--base-color);
+        color: var(--base);
         text-shadow: 1px 2px 0px black;
         margin-top: 15px;
     }
