0762ae4e800de967990e39fbe713b82d845b7892
Olliver-0 <nata.gabriel.de.oliveira@gmail.com>
2025-06-28 16:01:01 -0300

    Corrigindo erros de navegação

--- src/components/pop-up.js
+++ src/components/pop-up.js
@@ -30,7 +30,7 @@ const popUp = () => {
                         <input type="password" name="check-password" id="check-password">
                     </div>
                     <button type="submit" title="Enviar formulário">
-                        <img src="/public/img/botao-registrar.svg" alt="Enviar formulário">
+                        <img src="public/img/botao-registrar.svg" alt="Enviar formulário">
                     </button>
                 </form>
             </section>
